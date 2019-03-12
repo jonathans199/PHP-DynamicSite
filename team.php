18adfac6253d8dc4d1d6594c5663cfc15d704e6c
jonathan <jso1919@gmail.com>
2019-03-12 10:43:03 -0400

    added background

--- team.php
+++ team.php
@@ -12,20 +12,15 @@
 
   <?php foreach ($teamMembers as $member){
   ?>
-
     <div class="member">
       <img src="assets/img/<?php echo $member[img]; ?>" alt="<?php echo $member[name]; ?>">
       <h2><?php echo $member[name]; ?></h2>
       <p><?php echo $member[bio]; ?></p>
     </div>
-
   <?php 
         }
   ?>
 
 </div>
-
-
-
 
 <?php include('includes/footer.php');?>
